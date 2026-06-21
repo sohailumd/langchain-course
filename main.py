ff555382e4ab6561919b90be7dc739e884bc71fb
Sohail Mohammed <sohailumd@gmail.com>
2026-06-21 17:53:05 -0500

    Phase 1.1 completed

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ load_dotenv()
 def main():
     print("Hello from langchain-course!")
     information = """
-Lionel Andrés "Leo" Messi[note 1] (born 24 June 1987) is an Argentine professional footballer who plays as a forward for and captains both the Major League Soccer club Inter Miami and the Argentina national team. Widely regarded as one of the greatest players in history, Messi has set numerous records for individual accolades won throughout his professional footballing career, including eight Ballons d'Or, six European Golden Shoes, and eight times being named the world's best player by FIFA.[note 2] In 2025, he was named the All Time Men's World Best Player by the IFFHS.
-
-Messi is the most decorated player in the history of professional football, having won 46 team trophies.[note 3] His records include most goals in a calendar year (91), most goals for a single club (672 for Barcelona), most goals in La Liga (474), most assists in international football (61), most goal contributions in the FIFA World Cup (21), and most goal contributions in the Copa América (32). Messi has scored over 910 senior career goals and provided over 410 assists for club and country, resulting in over 1,320 goal contributions—the highest total in the sport's history.[25]
-"""
+FIFA 2026 will be the 23rd FIFA World Cup, the quadrennial international"""
     summary_template = """
 given the information {information} about a topic I want you to create:
 1. A short Summary
